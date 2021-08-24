31fd8bcaf4396b425eb803b0d354850cf8cd067e
Christoph Lindstädt <christophlindstaedt@web.de>
2021-08-24 10:20:09 +0200

    changed target to es6

--- src/entities/Boid.ts
+++ src/entities/Boid.ts
@@ -1,10 +1,10 @@
 import * as THREE from "three";
 
 export class Boid {
-  private _model: THREE.Mesh;
-  private _velocity = new THREE.Vector3();
+  #model: THREE.Mesh;
+  #velocity = new THREE.Vector3();
 
-  private _acceleration = new THREE.Vector3(
+  #acceleration = new THREE.Vector3(
     (Math.random() - 0.5) * 0.01,
     (Math.random() - 0.5) * 0.01,
     (Math.random() - 0.5) * 0.01
@@ -15,7 +15,7 @@ export class Boid {
     color = 0x0fd63d,
     wireframe = false,
   } = {}) {
-    this._model = new THREE.Mesh(
+    this.#model = new THREE.Mesh(
       new THREE.ConeGeometry(1, 3, 8),
       new THREE.MeshBasicMaterial({
         color: color,
@@ -23,25 +23,25 @@ export class Boid {
       })
     );
 
-    this._model.position.copy(
+    this.#model.position.copy(
       new THREE.Vector3(position.x - 0.5, position.y - 0.5, position.z - 0.5)
     );
   }
 
   update(this: Boid) {
-    this._velocity.add(this._acceleration);
-    this._velocity.clampLength(0, 0.2);
+    this.#velocity.add(this.#acceleration);
+    this.#velocity.clampLength(0, 0.2);
 
     //Quaternion Magic - https://stackoverflow.com/questions/9038465/three-js-object3d-cylinder-rotation-to-align-to-a-vector
-    this._model.quaternion.setFromUnitVectors(
+    this.#model.quaternion.setFromUnitVectors(
       new THREE.Vector3(0, 1, 0),
-      this._velocity.clone().normalize()
+      this.#velocity.clone().normalize()
     );
 
-    this._model.position.add(this._velocity);
+    this.#model.position.add(this.#velocity);
   }
 
   get model() {
-    return this._model;
+    return this.#model;
   }
 }
